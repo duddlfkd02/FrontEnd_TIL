bde2a38b03b91b23483c3b9b1e8452cdb00c3684
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-12-08 12:00:02 +0900

    update : App.js 탭 메뉴 기능 작업

--- Javascript/OnebiteProject/src/App.js
+++ Javascript/OnebiteProject/src/App.js
@@ -1,7 +1,43 @@
+import { request } from "./components/api.js";
 import Content from "./components/Content.js";
 import TabBar from "./components/TabBar.js";
 
 export default function App($app) {
-  const TabBar = new TabBar();
-  const Content = new Content();
+  this.state = {
+    currentTab: "all",
+    photos: [],
+  };
+
+  const tabBar = new TabBar({
+    initialState: "",
+    onClick: async () => {
+      this.setState({
+        ...this.state,
+        currentTab: name,
+        photos: await request(name),
+      });
+    },
+  });
+
+  const content = new Content();
+
+  this.setState = (newState) => {
+    this.state = newState;
+    tabBar.setState(this.state.currentTab);
+    content.setState(this.state.photos);
+  };
+
+  const init = async () => {
+    try {
+      const initialPhotos = await request();
+      this.setState({
+        ...this.state,
+        photos: initialPhotos,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  init();
 }
