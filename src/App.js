62e32bc025f00e390b7413714fa2df4d7fa16dba
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-12-11 16:35:48 +0900

    add : header 추가

--- src/App.js
+++ src/App.js
@@ -6,17 +6,85 @@ import RegionList from "./components/RegionList.js";
 import { requestData } from "./components/api.js";
 
 export default function App($app) {
+  const getSortBy = () => {
+    if (window.location.search) {
+      return window.location.search.split("sort=")[1].split("&")[0];
+    }
+    return "total";
+  };
+
+  const getSearchWord = () => {
+    if (window.location.search && window.location.search.includes("search=")) {
+      return window.location.search.split("search=")[1];
+    }
+    return "";
+  };
+
   this.state = {
     startIdx: 0,
-    sortBy: "",
-    searchWord: "",
+    sortBy: getSortBy(),
+    searchWord: getSearchWord(),
     region: "",
     cities: "",
   };
 
-  const header = new Header();
+  // 헤더
+  const header = new Header({
+    $app,
+    initialState: {
+      sortBy: this.state.sortBy,
+      searchWord: this.state.searchWord,
+    },
+    // 정렬 함수
+    handleSortChange: async (sortBy) => {
+      const pageUrl = `/${this.state.region}?sort=${sortBy}`;
+      history.pushState(
+        null,
+        null,
+        this.state.searchWord
+          ? pageUrl + `$search=${this.state.searchWord}`
+          : pageUrl
+      );
+      const cities = await requestData(
+        0,
+        this.state.region,
+        sortBy,
+        this.state.searchWord
+      );
+      this.setState({
+        ...this.state,
+        startIdx: 0,
+        sortBy: sortBy,
+        cities: cities,
+      });
+    },
+    // 검색 함수
+    handleSearch: async (searchWord) => {
+      history.pushState(
+        null,
+        null,
+        `/${this.state.region}?sort=${this.state.sortBy}&search=${searchWord}`
+      );
+
+      const cities = await requestData(
+        0,
+        this.state.region,
+        this.state.sortBy,
+        searchWord
+      );
+
+      this.setState({
+        ...this.state,
+        startIdx: 0,
+        cities: cities,
+        searchWord: searchWord,
+      });
+    },
+  });
+
   const regionList = new RegionList();
 
+  // 도시 리스트
   const cityList = new CityList({
     $app,
     initialState: this.state.cities,
@@ -40,9 +108,14 @@ export default function App($app) {
 
   const cityDetail = new CityDetail();
 
+  // 현재 상태 업데이트 함수
   this.setState = (newState) => {
     this.state = newState;
     cityList.setState(this.state.cities);
+    header.setState({
+      sortBy: this.state.sortBy,
+      searchWord: this.state.searchWord,
+    });
   };
 
   const init = async () => {
@@ -52,6 +125,7 @@ export default function App($app) {
       this.state.sortBy,
       this.state.searchWord
     );
+
     this.setState({
       ...this.state,
       cities: cities,
