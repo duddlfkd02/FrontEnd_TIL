43b677427a60c3dcfc9760c0f2567269a164f016
gimhayeong <duddlfkd02@gmail.com>
2024-12-20 16:04:31 +0900

    add : 북마크 리스트 보여주기

--- newTab/js/bookmark.js
+++ newTab/js/bookmark.js
@@ -1,4 +1,5 @@
 const newBookmarkForm = document.getElementById("bookmark-item-input-form");
+const bookmarkItemList = document.getElementById("bookmark-list");
 
 // 북마크 초기설정 : 로컬스토리지 저장
 let bookmarkList = [];
@@ -20,11 +21,47 @@ const newBookmarkToggle = () => {
 
 // 북마크 아이템 보여주기
 const setBookmarkItem = (item) => {
-  console.log(item);
+  const bookmarkItem = document.createElement("div");
+  bookmarkItem.classList.add("bookmark-item");
+  bookmarkItem.id = `bookmark-item-${item.createAt}`;
+
+  const bookmarkInfo = document.createElement("div");
+  bookmarkInfo.classList.add("bookmark-info");
+
+  const bookmarkUrl = document.createElement("a");
+  bookmarkUrl.classList.add("bookmark-url");
+
+  const urlIcon = document.createElement("div");
+  urlIcon.classList.add("url-icon");
+
+  const urlIconImage = document.createElement("img");
+
+  const nameElement = document.createElement("div");
+  nameElement.classList.add("url-name");
+
+  const bookmarkDeleteBtn = document.createElement("div");
+  bookmarkDeleteBtn.classList.add("del-btn");
+  bookmarkDeleteBtn.textContent = "삭제";
+
+  // 북마크 박스 만들기 (url 연결, 이미지, 사이트 명)
+  bookmarkUrl.href = item.url;
+  bookmarkUrl.target = "_blank";
+  urlIconImage.src = `https://www.google.com/s2/favicons?domain_url=${item.url}`;
+  nameElement.textContent = item.name;
+
+  bookmarkItem.appendChild(bookmarkInfo);
+  bookmarkItem.appendChild(bookmarkDeleteBtn);
+  bookmarkInfo.appendChild(bookmarkUrl);
+  bookmarkUrl.appendChild(urlIcon);
+  bookmarkUrl.appendChild(nameElement);
+  urlIcon.appendChild(urlIconImage);
+
+  bookmarkItemList.appendChild(bookmarkItem);
 };
 
 // 북마크 리스트 가져오기
 const setBookmarkList = () => {
+  bookmarkItemList.innerHTML = ""; // 기존 리스트 초기화
   bookmarkList.forEach((item) => {
     setBookmarkItem(item);
   });
@@ -40,14 +77,19 @@ const addBookmarkItem = () => {
   let url = document.getElementById("new-bookmark-url-input").value;
   let createAt = Date.now();
 
-  bookmarkList.push({ name: name, url: url, createAt: createAt });
+  const newItem = { name: name, url: url, createAt: createAt };
 
+  bookmarkList.push(newItem);
   localStorage.setItem("bookmarkList", JSON.stringify(bookmarkList));
 
   document.getElementById("new-bookmark-name-input").value = "";
   document.getElementById("new-bookmark-url-input").value = "";
+  setBookmarkItem(newItem);
   newBookmarkToggle();
 };
+
+// 초기 리스트 렌더링
+setBookmarkList();
 
 document
   .getElementById("bookmark-item-add-btn")
