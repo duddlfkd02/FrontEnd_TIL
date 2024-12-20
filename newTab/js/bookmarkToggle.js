bb9401b995e27c03b0132eeb885a8b078fd76746
gimhayeong <duddlfkd02@gmail.com>
2024-12-20 15:28:23 +0900

    refac : 북마크 토글 수정

--- newTab/js/bookmarkToggle.js
+++ newTab/js/bookmarkToggle.js
@@ -2,41 +2,43 @@ const bookmarkBar = document.getElementById("bookmark-bar");
 const bookMarkOpen = document.getElementById("bookmark-open");
 const bookMarkClose = document.getElementById("bookmark-close");
 
-// <--북마크 바-->
-const isBookMarkBarOpen = localStorage.getItem("isBookMarkBarOpen");
-if (isBookMarkBarOpen === "close") {
-  //isBookMarkBarOpen이 close
-  bookmarkBar.style.display = "none";
-  bookMarkOpen.style.display = "none";
-  bookMarkClose.style.display = "flex";
-} else {
-  //isBookMarkBarOpen이 open
-  bookmarkBar.style.display = "block";
-  bookMarkOpen.style.display = "flex";
-  bookMarkClose.style.display = "none";
+// 1. 초기 상태 설정하기
+let isBookMarkBarOpen = localStorage.getItem("isBookMarkBarOpen") === "true";
+
+// localStorage에 값이 없으면??
+if (isBookMarkBarOpen === null) {
+  isBookMarkBarOpen = false;
+  localStorage.setItem("isBookMarkBarOpen", "false");
 }
 
-// <--북마크 바 toggle-->
-const bookmarkBarToggle = () => {
-  let isBookMarkBarOpen = localStorage.getItem("isBookMarkBarOpen");
-
-  if (isBookMarkBarOpen === "close") {
-    localStorage.setItem("isBookMarkBarOpen", "open");
+// 2. UI 갱신 함수
+const updateBookmarkBarUI = () => {
+  if (isBookMarkBarOpen) {
+    // 열림
     bookmarkBar.style.display = "block";
     bookMarkOpen.style.display = "flex";
     bookMarkClose.style.display = "none";
-    return;
+  } else {
+    // 닫힘
+    bookmarkBar.style.display = "none";
+    bookMarkOpen.style.display = "none";
+    bookMarkClose.style.display = "flex";
   }
-
-  localStorage.setItem("isBookMarkBarOpen", "close");
-  bookmarkBar.style.display = "none";
-  bookMarkOpen.style.display = "none";
-  bookMarkClose.style.display = "flex";
 };
+
+// 3. 북마크 토글 함수
+const toggleBookmarkBar = () => {
+  isBookMarkBarOpen = !isBookMarkBarOpen;
+  localStorage.setItem("isBookMarkBarOpen", isBookMarkBarOpen);
+  updateBookmarkBarUI();
+};
+
+// 초기 UI 설정
+updateBookmarkBarUI();
 
 document
   .getElementById("bookmark-open-btn")
-  .addEventListener("click", bookmarkBarToggle);
+  .addEventListener("click", toggleBookmarkBar);
 document
   .getElementById("bookmark-close-btn")
-  .addEventListener("click", bookmarkBarToggle);
+  .addEventListener("click", toggleBookmarkBar);
