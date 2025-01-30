48e5bc82319e82d635aee6ede959f4e1b05d5a12
gimhayeong <duddlfkd02@gmail.com>
2025-01-30 16:16:36 +0900

    useReducer 사용

--- Typescript/section11/onebite-ts/src/App.tsx
+++ Typescript/section11/onebite-ts/src/App.tsx
@@ -1,19 +1,46 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useReducer, useRef } from "react";
 import Editor from "./components/Editor";
 import { Todo } from "./types";
 import TodoItem from "./components/TodoItem";
 
+type Action =
+  | {
+      type: "CREATE";
+      data: {
+        id: number;
+        content: string;
+      };
+    }
+  | { type: "DELETE"; id: number };
+
+function reducer(state: Todo[], action: Action) {
+  switch (action.type) {
+    case "CREATE": {
+      return [...state, action.data];
+    }
+    case "DELETE": {
+      return state.filter((item) => item.id !== action.id);
+    }
+  }
+}
+
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todos, dispatch] = useReducer(reducer, []);
 
   const idRef = useRef(0);
 
   const onClickAdd = (text: string) => {
-    setTodos([...todos, { id: idRef.current++, content: text }]);
+    dispatch({
+      type: "CREATE",
+      data: { id: idRef.current++, content: text },
+    });
   };
 
   const onClickDelete = (id: number) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
+    dispatch({
+      type: "DELETE",
+      id: id,
+    });
   };
 
   useEffect(() => {
