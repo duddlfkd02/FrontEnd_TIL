118e81c1ea28bc2d46158e5575f337b801da048e
gimhayeong <duddlfkd02@gmail.com>
2025-02-06 21:05:08 +0900

    데이터 fetch, scss로 로딩/에러 화면 작업

--- React/wedding-app/src/App.tsx
+++ React/wedding-app/src/App.tsx
@@ -1,10 +1,45 @@
 import classNames from "classnames/bind";
 import styles from "./scss/App.module.scss";
+import { useEffect, useState } from "react";
+
+import FullScreenMessage from "./components/shared/FullScreenMessage";
 
 const cx = classNames.bind(styles);
 
 function App() {
-  return <div className={cx("container")}>안녕하세요</div>;
+  const [wedding, setWedding] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("http://localhost:8888/wedding2")
+      .then((response) => {
+        if (response.ok === false) {
+          throw new Error("청첩장 정보를 불러오지 못했습니다.");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setWedding(data);
+      })
+      .catch((error) => {
+        console.log("error", error);
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <FullScreenMessage type="loading" />;
+  }
+  if (error) {
+    return <FullScreenMessage type="error" />;
+  }
+
+  return <div className={cx("container")}>{JSON.stringify(wedding)}</div>;
 }
 
 export default App;
