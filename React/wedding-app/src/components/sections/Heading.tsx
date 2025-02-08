375f0c1c986cc7992d8350eb861c6d6bcf842319
gimhayeong <duddlfkd02@gmail.com>
2025-02-08 16:39:54 +0900

    add : section 컴포넌트, reset.css 추가

---
+++ React/wedding-app/src/components/sections/Heading.tsx
@@ -0,0 +1,12 @@
+import classNames from "classnames/bind";
+import styles from "./Section.module.scss";
+
+import Section from "@shared/Section";
+
+const cx = classNames.bind(styles);
+
+function Heading() {
+  return <Section>Heading</Section>;
+}
+
+export default Heading;
---
+++ React/wedding-app/src/components/sections/Video.tsx
@@ -0,0 +1,10 @@
+import classNames from "classnames/bind";
+import styles from "./Vedio.module.scss";
+
+import Section from "@shared/Section";
+
+const cx = classNames.bind(styles);
+
+function Vedio() {}
+
+export default Vedio;
