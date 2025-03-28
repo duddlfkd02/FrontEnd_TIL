fb7f9012b385af2d203ca56ab75c4d971bb92cf7
gimhayeong <duddlfkd02@gmail.com>
2025-03-28 11:29:18 +0900

    section2 스타일링까지 완료

--- Next/ob-next/section02/src/pages/_app.tsx
+++ Next/ob-next/section02/src/pages/_app.tsx
@@ -1,6 +1,29 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  const onClickButton = () => {
+    router.push("/test");
+  };
+
+  return (
+    <>
+      <header>
+        <Link href={"/"}>Home</Link>
+        &nbsp;
+        <Link href={"/search"} prefetch={false}>
+          Search
+        </Link>
+        &nbsp;
+        <Link href={"/book"}>Book</Link>
+        <div>
+          <button onClick={onClickButton}>테스트페이지</button>
+        </div>
+      </header>
+      <Component {...pageProps} />;
+    </>
+  );
 }
